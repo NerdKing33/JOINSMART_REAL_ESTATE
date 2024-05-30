410f0d8a1bc1fd86200ed43c8b43dc6212cb1d83
Oliver <kwamewilfrednk@gmail.com>
2024-05-30 23:07:58 +0300

    This is the First draft from which a big ui change

---
+++ lib/impVariable.dart
@@ -0,0 +1,88 @@
+import 'package:flutter/material.dart';
+
+FontWeight w100 = FontWeight.w100;
+FontWeight w200 = FontWeight.w200;
+FontWeight w300 = FontWeight.w300;
+FontWeight w400 = FontWeight.w400;
+FontWeight w500 = FontWeight.w500;
+FontWeight w600 = FontWeight.w600;
+FontWeight w700 = FontWeight.w700;
+FontWeight w800 = FontWeight.w800;
+FontWeight w900 = FontWeight.w900;
+FontWeight wBold = FontWeight.bold;
+FontWeight wNrml = FontWeight.normal;
+
+String jeLogo = 'lib/images/jeLogo.jpg';
+String constructionImage = 'lib/images/constructionImage.png';
+String title = 'Joinsmart Estates';
+String titleCptl = 'JOINSMART ESTATES ';
+
+SizedBox sb0 = const SizedBox(width: 0);
+SizedBox sb5 = const SizedBox(width: 5);
+SizedBox sb10 = const SizedBox(width: 10,);
+SizedBox sb15 = const SizedBox(width: 15,);
+SizedBox sb20 = const SizedBox(width: 20,);
+SizedBox sb25 = const SizedBox(width: 25,);
+SizedBox sb30 = const SizedBox(width: 30,);
+SizedBox sb35 = const SizedBox(width: 35,);
+SizedBox sb40 = const SizedBox(width: 40,);
+SizedBox sb45 = const SizedBox(width: 45,);
+SizedBox sb50 = const SizedBox(width: 50,);
+SizedBox sb55 = const SizedBox(width: 55,);
+SizedBox sb60 = const SizedBox(width: 60,);
+SizedBox sb65 = const SizedBox(width: 65,);
+SizedBox sb70 = const SizedBox(width: 70,);
+SizedBox sb75 = const SizedBox(width: 75,);
+SizedBox sb80 = const SizedBox(width: 80,);
+SizedBox sb85 = const SizedBox(width: 85,);
+SizedBox sb90 = const SizedBox(width: 90,);
+SizedBox sb95 = const SizedBox(width: 95,);
+SizedBox sb100 = const SizedBox(width: 100,);
+
+SizedBox sbH5 = const SizedBox(height: 5);
+SizedBox sbH8 = const SizedBox(height: 8);
+SizedBox sbH10 = const SizedBox(height: 10,);
+SizedBox sbH15 = const SizedBox(height: 15,);
+SizedBox sbH20 = const SizedBox(height: 20,);
+SizedBox sbH25 = const SizedBox(height: 25,);
+SizedBox sbH30 = const SizedBox(height: 30,);
+SizedBox sbH35 = const SizedBox(height: 35,);
+SizedBox sbH40 = const SizedBox(height: 40,);
+SizedBox sbH45 = const SizedBox(height: 45,);
+SizedBox sbH50 = const SizedBox(height: 50,);
+SizedBox sbH55 = const SizedBox(height: 55,);
+SizedBox sbH60 = const SizedBox(height: 60,);
+SizedBox sbH65 = const SizedBox(height:  65,);
+SizedBox sbH70 = const SizedBox(height: 70,);
+SizedBox sbH75 = const SizedBox(height: 75,);
+SizedBox sbH80 = const SizedBox(height: 80,);
+SizedBox sbH85 = const SizedBox(height: 85,);
+SizedBox sbH90 = const SizedBox(height: 90,);
+SizedBox sbH95 = const SizedBox(height: 95,);
+SizedBox sbH100 = const SizedBox(height: 100,);
+
+LinearGradient blueGradient = LinearGradient(
+  begin: Alignment.topCenter,
+  end: Alignment.center,
+  colors: [
+    Colors.blue.shade800,
+    Colors.blue.shade700,
+    Colors.blue.shade600,
+  ],
+  transform:  const GradientRotation(.0),
+);
+
+List<BoxShadow> basicShadow = [
+  BoxShadow(
+      color: Colors.grey.shade500,
+      offset: const Offset(4.0, 4.0),
+      blurRadius: 10.0,
+      spreadRadius: 1.0
+  ),
+  BoxShadow(
+      color: Colors.grey.shade400,
+      offset: const Offset(4.0, 4.0),
+      blurRadius: 10.0,
+      spreadRadius: 1.0
+  )
+];
